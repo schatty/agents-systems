939fa9f0e19082b4abbc5189280cf419a17a3ca2
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2019-11-11 14:06:32 +0300

    dm_suite wrapper fix

--- env/dm_env_wrapper.py
+++ env/dm_env_wrapper.py
@@ -5,7 +5,7 @@ import numpy as np
 class DMSuiteEnvWrapper:
     def __init__(self, env_name):
         self.env_name, self.task = env_name.split('-')
-        self.env = suite.load(domain_name=env_name, task_name=self.task)
+        self.env = suite.load(domain_name=self.env_name, task_name=self.task)
 
     def reset(self):
         time_step = self.env.reset()
@@ -15,7 +15,7 @@ class DMSuiteEnvWrapper:
                 next_state += [v]
             else:
                 next_state += v.tolist()
-        return next_state
+        return np.array(next_state)
 
     def get_random_action(self):
         action = self.env.action_space.sample()
@@ -38,7 +38,7 @@ class DMSuiteEnvWrapper:
         except:
             reward = 0
 
-        return next_state, reward, 0
+        return np.array(next_state), reward, 0
 
     def set_random_seed(self, seed):
         self.env.seed(seed)
